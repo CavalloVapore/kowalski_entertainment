87f8f9ec81a63a4b7069ac9d20780ace460663ca
Alex <Laex410@gmail.com>
2015-05-02 00:06:20 +0200

    Important Update!!!!! Rotation Script. Walentin sucks. Totally. That asshole. Fuck him. Fuck this.

---
+++ kowalski_entertainment/Assets/Scripts/Objects/SpinThatShit.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpinThatShit : MonoBehaviour {
+
+    public enum chooseRotation
+    {
+        x,
+        y,
+        z
+    }
+    public chooseRotation myRot;
+    public float speed;
+
+	// Use this for initialization
+	void Start () {
+        myRot = chooseRotation.x;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        switch (myRot)
+        {
+            case chooseRotation.x:
+            {
+                this.transform.Rotate(new Vector3(speed, 0, 0)*Time.deltaTime);
+            }
+            break;
+            case chooseRotation.y:
+            {
+                this.transform.Rotate(new Vector3(0, speed, 0) * Time.deltaTime);
+            }
+            break;
+            case chooseRotation.z:
+            {
+                this.transform.Rotate(new Vector3(0, 0, speed) * Time.deltaTime);
+            }
+            break;
+        }
+	}
+}
