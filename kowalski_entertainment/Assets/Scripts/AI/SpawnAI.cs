87c39592072897803e9e63e42e8b81c5c8bb70ce
CavalloVapore <junk.gamersjunk@googlemail.com>
2015-05-01 19:46:31 +0200

    test

---
+++ kowalski_entertainment/Assets/Scripts/AI/SpawnAI.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnAI : MonoBehaviour 
+{
+
+    public Transform spawnPoint1;
+    public Transform spawnPoint2;
+    public Transform spawnPoint3;
+    public Transform spawnPoint4;
+    public Transform spawnPoint5;
+    public Transform spawnPoint6;
+    public Transform spawnPoint7;
+    public Transform spawnPoint8;
+    public Transform spawnPoint9;
+    public Transform spawnPoint10;
+
+	// Use this for initialization
+	void Start () 
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	
+	}
+}
