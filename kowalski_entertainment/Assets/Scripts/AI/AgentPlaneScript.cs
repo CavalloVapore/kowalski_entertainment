921b85ea3befe4a70ac729977fea0fdada541d49
CavalloVapore <junk.gamersjunk@googlemail.com>
2015-05-02 09:25:16 +0200

    air plane AI working

---
+++ kowalski_entertainment/Assets/Scripts/AI/AgentPlaneScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AgentPlaneScript : MonoBehaviour 
+{
+
+    public Transform target1;
+    public Transform target2;
+    public Transform target3;
+    public Transform target4;
+
+    NavMeshAgent agent;
+
+    // Use this for initialization
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.SetDestination(target3.position);
+    }
+}
