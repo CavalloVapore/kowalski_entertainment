4c7b8edd3a3e0e55fc304a83ae0a9ea9754e52ec
CavalloVapore <junk.gamersjunk@googlemail.com>
2015-05-02 16:24:00 +0200

    ui healthbar working

--- kowalski_entertainment/Assets/Scripts/AI/UnitsUI.cs
+++ kowalski_entertainment/Assets/Scripts/AI/UnitsUI.cs
@@ -1,14 +1,21 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class UnitsUI : MonoBehaviour {
 
     private Transform target;
     private GameObject mainCamera;
+    private Slider healthbar;
+    private Enemy enemyUnit = new Enemy();
 
     void Start()
     {
         mainCamera = GameObject.FindGameObjectWithTag("MainCamera");
+
+        enemyUnit = gameObject.GetComponentInParent<Enemy>();
+        healthbar = gameObject.GetComponentInChildren<Slider>();
+        healthbar.maxValue = enemyUnit.enemyHealth;
         if (mainCamera)
             target = mainCamera.transform;
         else
@@ -17,7 +24,7 @@ public class UnitsUI : MonoBehaviour {
 
     void LateUpdate()
     {
-        //TODO: try to do it only if needed
         transform.LookAt(target);
+        healthbar.value = enemyUnit.enemyHealth;
     }    
 }
