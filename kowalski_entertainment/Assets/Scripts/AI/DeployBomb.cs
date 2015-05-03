ccba092b50a38d1f8ddb75474894a19e66aa0962
CavalloVapore <junk.gamersjunk@googlemail.com>
2015-05-03 11:59:00 +0200

    changes

---
+++ kowalski_entertainment/Assets/Scripts/AI/DeployBomb.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DeployBomb : MonoBehaviour {
+
+    public Detonator bomb;
+
+    void OnTriggerEnter(Collider other)
+    {
+        bomb.Explode();
+    }
+}
