a244a4c89f92c47e2c1f99663ecea211a8e95fe4
Alex <Laex410@gmail.com>
2015-05-03 02:57:40 +0200

    update

--- kowalski_entertainment/Assets/Scripts/Network/Bunker.cs
+++ kowalski_entertainment/Assets/Scripts/Network/Bunker.cs
@@ -48,6 +48,9 @@ public class Bunker : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+
+        if (Input.GetKeyDown(KeyCode.K))
+            Debug.Log(occ1);
             
     }
 
