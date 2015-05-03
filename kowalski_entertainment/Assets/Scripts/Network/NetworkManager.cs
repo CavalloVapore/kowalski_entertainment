d9a0b543da5f472c76aff842c7af64f59e547ef0
CavalloVapore <junk.gamersjunk@googlemail.com>
2015-05-03 08:42:01 +0200

    added plane

--- kowalski_entertainment/Assets/Scripts/Network/NetworkManager.cs
+++ kowalski_entertainment/Assets/Scripts/Network/NetworkManager.cs
@@ -23,6 +23,15 @@ public class NetworkManager : MonoBehaviour
     public Transform spawnPoint7;
     public Transform spawnPoint8;
 
+    public Transform spawnPointAir1;
+    public Transform spawnPointAir2;
+    public Transform spawnPointAir3;
+    public Transform spawnPointAir4;
+    public Transform spawnPointAir5;
+    public Transform spawnPointAir6;
+    public Transform spawnPointAir7;
+    public Transform spawnPointAir8;
+
     public GameObject startButton;
     public GameObject joinButton;
     public Canvas canvas;
@@ -30,6 +39,7 @@ public class NetworkManager : MonoBehaviour
     public GameObject AI1;
     public GameObject AI2;
     public GameObject AI3;
+    public GameObject AI4;
 
     private int count = 0;
     private int random;
@@ -86,7 +96,7 @@ public class NetworkManager : MonoBehaviour
             for (int i = 0; i <= 5; i++)
             {
                 random = Random.Range(1, 9);
-                random2 = Random.Range(0, 3);
+                random2 = Random.Range(0, 4);
 
                 switch (random2)
                 {
@@ -197,6 +207,59 @@ public class NetworkManager : MonoBehaviour
 
                             case (8):
                                 Network.Instantiate(AI3, spawnPoint8.position, Quaternion.identity, 0);
+                                break;
+                        }
+                        break;
+
+                        case (3):
+                        switch (random)
+                        {
+                            case (1):
+                                int[] ran1 = { 3,4,5,6,7,8};
+                                GameObject plane1 = (GameObject)Network.Instantiate(AI4, spawnPointAir1.position, Quaternion.identity, 0);
+                                plane1.GetComponent<AgentPlaneScript>().chooseTarget = ran1[Random.Range(0, 6)];
+                                break;
+
+                            case (2):
+                                int[] ran2 = { 3, 4, 5, 6, 7, 8 };
+                                GameObject plane2 = (GameObject)Network.Instantiate(AI4, spawnPointAir2.position, Quaternion.identity, 0);
+                                plane2.GetComponent<AgentPlaneScript>().chooseTarget = ran2[Random.Range(0, 6)];
+                                break;
+
+                            case (3):
+                                int[] ran3 = { 1,2,4,6,7,8 };
+                                GameObject plane3 = (GameObject)Network.Instantiate(AI4, spawnPointAir3.position, Quaternion.identity, 0);
+                                plane3.GetComponent<AgentPlaneScript>().chooseTarget = ran3[Random.Range(0, 6)];
+                                break;
+
+                            case (4):
+                                int[] ran4 = { 1,2,3,5,6,7 };
+                                GameObject plane4 = (GameObject)Network.Instantiate(AI4, spawnPointAir4.position, Quaternion.identity, 0);
+                                plane4.GetComponent<AgentPlaneScript>().chooseTarget = ran4[Random.Range(0, 6)];
+                                break;
+
+                            case (5):
+                                int[] ran5 = { 1, 2, 4, 6, 7, 8 };
+                                GameObject plane5 = (GameObject)Network.Instantiate(AI4, spawnPointAir5.position, Quaternion.identity, 0);
+                                plane5.GetComponent<AgentPlaneScript>().chooseTarget = ran5[Random.Range(0, 6)];
+                                break;
+
+                            case (6):
+                                int[] ran6 = { 1,2,3,4,5,8};
+                                GameObject plane6 = (GameObject)Network.Instantiate(AI4, spawnPointAir6.position, Quaternion.identity, 0);
+                                plane6.GetComponent<AgentPlaneScript>().chooseTarget = ran6[Random.Range(0, 6)];
+                                break;
+
+                            case (7):
+                                int[] ran7 = { 1, 2, 3, 4, 5, 8 };
+                                GameObject plane7 = (GameObject)Network.Instantiate(AI4, spawnPointAir7.position, Quaternion.identity, 0);
+                                plane7.GetComponent<AgentPlaneScript>().chooseTarget = ran7[Random.Range(0, 6)];
+                                break;
+
+                            case (8):
+                                int[] ran8 = { 1, 2, 3, 5, 6, 7 };
+                                GameObject plane8 = (GameObject)Network.Instantiate(AI4, spawnPointAir8.position, Quaternion.identity, 0);
+                                plane8.GetComponent<AgentPlaneScript>().chooseTarget = ran8[Random.Range(0, 6)];
                                 break;
                         }
                         break;
